1476210d78446e74986c87ba64af3770beebc8ab
Yusuf <y.abdulla37@gmail.com>
2023-03-13 00:31:42 -0600

    fix settings

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -127,7 +127,6 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
